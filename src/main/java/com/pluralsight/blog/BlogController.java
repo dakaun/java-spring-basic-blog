e81045c3679323d0aa28a0e495378824266b6b83
dakaun <daniela.kaun@gmx.de>
2021-07-06 09:37:23 +0200

    module 2 implemented (display data from post repo)

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,29 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
+	private PostRepository postRepository;
+	
+	public BlogController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
 	
 	@RequestMapping("/")
 	public String listPosts(ModelMap modelmap) {
-		modelmap.put("title", "Blog Post 1");
+		List<Post> allPostings = postRepository.getAllPosts();
+		modelmap.put("posts", allPostings);
 		return "home";
 	}
+	
+	
 
 }
